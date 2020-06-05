ec90fa5258959c12f9349b99be380a68a37ecf0a
Alex Standke <astandke@vmware.com>
2020-06-05 18:03:24 +0000

    add logging

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -8,10 +9,12 @@ import (
 
 func main() {
 	http.HandleFunc("/", foo)
+	log.Printf("Server started")
 	_ = http.ListenAndServe(":8080", nil)
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Got a request...")
 	w.Header().Set("Server", "A Go Web Server")
 	code, _ := strconv.Atoi(os.Getenv("RESPONSE_STATUS"))
 	w.WriteHeader(code)
